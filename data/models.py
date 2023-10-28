864b9ef142d434561ee3f8df38f5fe8bf1ab06ee
Eric Gusmão <ericdegusmaopino@gmail.com>
2023-10-28 17:50:46 -0300

    Adiciona classe Enum para faixa

--- data/models.py
+++ data/models.py
@@ -1,6 +1,18 @@
+from enum import IntEnum
 from typing import List, Optional
 from sqlalchemy import UniqueConstraint
-from sqlmodel import Field, Relationship, SQLModel
+from sqlmodel import Field, Relationship, SQLModel, Enum, Column
+
+
+class TypeFaixa(IntEnum):
+    branca = 1
+    amarela = 2
+    vermelha = 3
+    laranja = 4
+    verde = 5
+    roxa = 6
+    marrom = 7
+    preta = 8
 
 
 class Turma(SQLModel, table=True):
@@ -16,6 +28,6 @@ class Aluno(SQLModel, table=True):
     nome: str
     idade: int
     cpf: str
-    faixa: str  # [branca, amarela, vermelha, laranja, verde, roxa, marrom, preta] TODO: validar
+    faixa: TypeFaixa = Field(sa_column=Column(Enum(TypeFaixa)), nullable=False)
     turma_id: Optional[int] = Field(foreign_key="turma.id", default=None)
     turma: Optional[Turma] = Relationship(back_populates="alunos")
