33a2a15be98b0628f86464be2692107b6f3baf94
Eric Gusmão <ericdegusmaopino@gmail.com>
2023-11-19 17:08:44 -0300

    [turma] adiciona comando listar-alunos

--- apps/turma.py
+++ apps/turma.py
@@ -3,15 +3,30 @@ from sqlmodel import Session, select
 from rich.table import Table
 from rich import print
 from data.database import engine
-from data.models import Turma
+from data.models import Turma, TypeFaixa
+from utils.queries import get_turma_by_id
 
 app = typer.Typer()
 
 
-@app.command()
-def listar_alunos():
-    # TODO: Mostra uma lista com todos alunos(nome, cpf e faixa) (id da turma como argumento)
-    pass
+@app.command("listar-alunos")
+def listar_alunos(id: int):
+    with Session(engine) as session:
+        turma = get_turma_by_id(id, session)
+        if not turma:
+            return print("[bold red]Turma não encontrada! :( [/bold red]")
+        alunos = turma.alunos
+        if len(alunos) <= 0:
+            return print("[bold red]Não há alunos nessa turma :( [/bold red]")
+        tabela = Table(title=f"{turma.nome_turma}", show_lines=True)
+        tabela.add_column("CPF", style="cyan")
+        tabela.add_column("Nome", style="magenta")
+        tabela.add_column("Faixa", style="green")
+        for aluno in alunos:
+            tabela.add_row(
+                f"{aluno.cpf}", f"{aluno.nome}", f"{TypeFaixa(aluno.faixa).name}"
+            )
+        print(tabela)
 
 
 @app.command()
