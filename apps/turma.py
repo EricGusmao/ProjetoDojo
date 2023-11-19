efb33debeb01d07cf975cdf6bd9c038fd137abc4
Eric Gusmão <ericdegusmaopino@gmail.com>
2023-11-19 14:32:38 -0300

    [Turma] Adiciona comando listar

--- apps/turma.py
+++ apps/turma.py
@@ -1,5 +1,7 @@
 import typer
-from sqlmodel import Session
+from sqlmodel import Session, select
+from rich.table import Table
+from rich import print
 from data.database import engine
 from data.models import Turma
 
@@ -10,3 +12,16 @@ app = typer.Typer()
 def listar_alunos():
     # TODO: Mostra uma lista com todos alunos(nome, cpf e faixa) (id da turma como argumento)
     pass
+
+
+@app.command()
+def listar():
+    with Session(engine) as session:
+        turmas = session.exec(select(Turma)).all()
+        tabela = Table(title="Turmas", show_lines=True)
+        tabela.add_column("ID", style="cyan")
+        tabela.add_column("Nome da Turma", style="magenta")
+        tabela.add_column("Qtd de Alunos", style="green")
+        for turma in turmas:
+            tabela.add_row(f"{turma.id}", f"{turma.nome_turma}", f"{len(turma.alunos)}")
+        print(tabela)
