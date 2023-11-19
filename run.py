2b2536ac35cdfa6987698f7d8a0861a068bbc52d
Eric Gusmão <ericdegusmaopino@gmail.com>
2023-11-19 17:04:15 -0300

    [run.py] Adiciona funcao gerar_alunos

--- run.py
+++ run.py
@@ -5,6 +5,22 @@ from data.database import create_db_and_tables, engine
 from sqlmodel import Session, select
 
 
+def gerar_alunos():
+    with open("tests/pessoas.json", "r") as pessoas_file:
+        pessoas = json.load(pessoas_file)
+    with Session(engine) as session:
+        for pessoa in pessoas:
+            novo_aluno = Aluno(
+                nome=pessoa["nome"],
+                faixa=pessoa["faixa"],
+                idade=pessoa["idade"],
+                cpf=pessoa["cpf"],
+                turma_id=pessoa["turma_id"],
+            )
+            session.add(novo_aluno)
+        session.commit()
+
+
 def criar_turmas():
     with Session(engine) as session:
         turmas = session.exec(select(Turma)).all()
